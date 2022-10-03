245f358251d4a92374c4a0963e87555ab0d561c6
tigaron <suputra@mail.com>
2022-10-03 21:44:24 +0800

    feat: mvp - login and callback for keycloak

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 const { NODE_ENV, PORT } = process.env;
 const express = require("express");
 const morgan = require("morgan");
+const fs = require("fs");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
+const { Strategy } = require("passport-saml");
 
 async function errorMiddleware(error, req, res, next) {
   try {
@@ -25,6 +30,24 @@ class IndexController {
   }
 }
 
+class LoginController {
+  async login(req, res, next) {
+    try {
+      next();
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async callback(req, res, next) {
+    try {
+      res.send(req.user);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
 class IndexRoute {
   constructor() {
     this.path = "/";
@@ -38,16 +61,6 @@ class IndexRoute {
   }
 }
 
-class LoginController {
-  async login(req, res, next) {
-    try {
-      res.sendStatus(200);
-    } catch (error) {
-      next(error);
-    }
-  }
-}
-
 class LoginRoute {
   constructor() {
     this.path = "/login";
@@ -57,7 +70,17 @@ class LoginRoute {
   }
 
   initializeRoutes() {
-    this.router.get(`${this.path}`, this.loginController.login);
+    this.router.get(
+      `${this.path}`,
+      this.loginController.login,
+      passport.authenticate("samlStrategy")
+    );
+    this.router.post(
+      `${this.path}/callback`,
+      this.loginController.login,
+      passport.authenticate("samlStrategy"),
+      this.loginController.callback
+    );
   }
 }
 
@@ -66,27 +89,63 @@ class App {
     this.app = express();
     this.env = NODE_ENV || "development";
     this.port = PORT || 3000;
+    this.samlConfig = {
+      issuer: "express-kc",
+      entityId: "SAML-SSO-App",
+      callbackUrl: "https://localhost:5000/login/callback",
+      signOut: "https://localhost:5000/logout/callback",
+      entryPoint: "https://localhost:8443/realms/test/protocol/saml",
+    };
 
     this.initializeMiddlewares();
+    this.initializePassport();
     this.initializeRoutes(routes);
     this.initializeErrorHandling();
   }
 
-  listen() {
-    this.app.listen(this.port, () => {
-      console.log(`======= ENV: ${this.env} =======`);
-      console.log(`🚀 App listening on the port ${this.port}`);
-    });
-  }
-
-  getServer() {
-    return this.app;
-  }
-
   initializeMiddlewares() {
     this.app.use(morgan("short"));
+    this.app.use(cookieParser());
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
+  }
+
+  initializePassport() {
+    this.app.use(
+      session({
+        secret: "secret",
+        resave: false,
+        saveUninitialized: true,
+      })
+    );
+    passport.use(
+      "samlStrategy",
+      new Strategy(
+        {
+          callbackUrl: this.samlConfig.callbackUrl,
+          entryPoint: this.samlConfig.entryPoint,
+          issuer: this.samlConfig.issuer,
+          identifierFormat: null,
+          decryptionPvk: fs.readFileSync("sp-pvt.key.pem", "utf8"),
+          cert: fs.readFileSync("idp-pub.key.pem", "utf8"),
+          privateKey: fs.readFileSync("sp-pvt.key.pem", "utf8"),
+          validateInResponseTo: true,
+          disableRequestedAuthnContext: true,
+        },
+        (profile, done) => {
+          console.log("passport.use() profile: %s \n", JSON.stringify(profile));
+          return done(null, profile);
+        }
+      )
+    );
+    passport.serializeUser(function (user, done) {
+      console.log("passport.serializeUser(): %s \n", JSON.stringify(user));
+      done(null, user);
+    });
+    passport.deserializeUser(function (user, done) {
+      console.log("passport.deserializeUser(): %s \n", JSON.stringify(user));
+      done(null, user);
+    });
   }
 
   initializeRoutes(routes) {
@@ -97,6 +156,17 @@ class App {
 
   initializeErrorHandling() {
     this.app.use(errorMiddleware);
+  }
+
+  getServer() {
+    return this.app;
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(`======= ENV: ${this.env} =======`);
+      console.log(`🚀 App listening on the port ${this.port}`);
+    });
   }
 }
 
